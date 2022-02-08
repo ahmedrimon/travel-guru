6f49618edf14b91f4f322d3f3730c6517d061d8f
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-02-08 17:11:35 +0600

    added file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
@@ -20,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+/* basename={process.env.PUBLIC_URL} */
